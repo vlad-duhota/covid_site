9d8a0ac388bc4893ce8e87538151221fffc25ca7
vlad-duhota <vladarduino@gmail.com>
2022-02-18 21:32:30 +0200

    js object added

--- main.js
+++ main.js
@@ -17,36 +17,54 @@ $(document).ready(function () {
     }
 });
 
+// conditions object
+let conditions = {
+    // percents
+    nums: {
+        num1: 16,
+        num2: 36,
+        num3: 60,
+    },
+    // flags
+    objects_flags: {
+        num1: '.flag_1',
+        num2: '.flag_2',
+        num3: '.flag_3',
+    },
+    // text list items
+    objects_items: {
+        num1: '.car__item_1',
+        num2: '.car__item_2',
+        num3: '.car__item_3',
+    }
+}
+
+// animation function
 function anim() {
     $(window).scroll(function () {
         var scrollY = window.scrollY - 175;
+        // if user scrolled to anim section
         if (scrollY > 0) {
+            // calc value
             var width = $('.car').css('width').replace('px', '');
             var value = scrollY / width * 350;
             $('.car-img').css("left", value * .3 + "%")
             $('.car').addClass('fixed');
-            if (value * .3 > 16) {
-                $('.flag_1').addClass('active');
-                $('.car__item_1').addClass('active');
-            } else {
-                $('.flag_1').removeClass('active');
-                $('.car__item_1').removeClass('active');
+            // conditions check
+            for (let key in conditions.nums) {
+                if (value * .3 > conditions.nums[key]) {
+                    // add classes if condition true
+                    $(conditions.objects_flags[key]).addClass('active');
+                    $(conditions.objects_items[key]).addClass('active');
+                }else{
+                    // remove classes if condition false
+                    $(conditions.objects_flags[key]).removeClass('active');
+                    $(conditions.objects_items[key]).removeClass('active');
+                }
             }
-            if (value * .3 > 36) {
-                $('.flag_2').addClass('active');
-                $('.car__item_2').addClass('active');
-            } else {
-                $('.flag_2').removeClass('active');
-                $('.car__item_2').removeClass('active');
-            }
-            if (value * .3 > 60) {
-                $('.flag_3').addClass('active');
-                $('.car__item_3').addClass('active');
-            } else {
-                $('.flag_3').removeClass('active');
-                $('.car__item_3').removeClass('active');
-            }
+            // if anim has been ended
             if (value * .3 > 84) {
+                // null values and remove class
                 $('.car').removeClass('fixed');
                 value = 0
             }
@@ -95,14 +113,14 @@ faq_observer.observe(document.querySelector('.faq__img'));
 
 // pop up form
 
-function form(has_next, has_prev , th, open_btn, close_btn, prev_btn, next_btn) {
+function form(has_next, has_prev, th, open_btn, close_btn, prev_btn, next_btn) {
     if (has_next) {
         $(th).change(function () {
             $(next_btn).addClass('active');
         });
     }
-    if(has_prev){
-        $(prev_btn).click(function(e){
+    if (has_prev) {
+        $(prev_btn).click(function (e) {
             e.preventDefault();
             $(th).removeClass('active');
         });
@@ -118,6 +136,6 @@ function form(has_next, has_prev , th, open_btn, close_btn, prev_btn, next_btn) 
 }
 
 // call form functions
-form(true , false, '.pop-up_1', '.book', '.close-btn_1' , '', '.pop-up__next_1');
-form(true , true, '.pop-up_2', '.pop-up__next_1', '.close-btn_2', '.pop-up__prev_1', '.pop-up__next_2');
-form(false , true, '.pop-up_3', '.pop-up__next_2', '.close-btn_3', '.pop-up__prev_2', '');
+form(true, false, '.pop-up_1', '.book', '.close-btn_1', '', '.pop-up__next_1');
+form(true, true, '.pop-up_2', '.pop-up__next_1', '.close-btn_2', '.pop-up__prev_1', '.pop-up__next_2');
+form(false, true, '.pop-up_3', '.pop-up__next_2', '.close-btn_3', '.pop-up__prev_2', '');
